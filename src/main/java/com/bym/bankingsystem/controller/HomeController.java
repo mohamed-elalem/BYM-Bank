78b7a458163268d1bd715872a4dce5d670c4c1f1
bekijerry <bekigher@gmail.co>
2020-04-11 20:59:44 -0500

    changes in properties file and home controller added and swagger added

---
+++ src/main/java/com/bym/bankingsystem/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.bym.bankingsystem.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HomeController {
+
+    @GetMapping("/")
+    public void gethome(){
+
+    }
+}
