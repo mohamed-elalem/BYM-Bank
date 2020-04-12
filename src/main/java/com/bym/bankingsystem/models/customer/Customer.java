440f1c3882e86bfe7570d325732592f482734c03
bekijerry <bekigher@gmail.co>
2020-04-11 21:58:02 -0500

    attributes are added in account and customer class

--- src/main/java/com/bym/bankingsystem/models/customer/Customer.java
+++ src/main/java/com/bym/bankingsystem/models/customer/Customer.java
@@ -1,13 +1,42 @@
 package com.bym.bankingsystem.models.customer;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 
 @Entity
 public class Customer {
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Id
     private Long id;
+
+    @NotBlank
+    @Column(name = "customer_name", nullable = false)
+    private String customerName;
+
+    @NotBlank
+    @Column(name = "customer_number", nullable = false, unique = true)
+    private String customerNumber;
+
+    @NotBlank
+    @Column(name = "contact_phone", nullable = false)
+    private String contactPhone;
+
+    public Customer() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public String getCustomerNumber() {
+        return customerNumber;
+    }
+
+    public String getContactPhone() {
+        return contactPhone;
+    }
 }
--- src/main/java/com/bym/bankingsystem/models/transaction/Transaction.java
+++ src/main/java/com/bym/bankingsystem/models/transaction/Transaction.java
@@ -1,13 +1,56 @@
 package com.bym.bankingsystem.models.transaction;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import com.bym.bankingsystem.models.account.AccountType;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 
 @Entity
 public class Transaction {
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Id
     private Long id;
+
+    @NotBlank
+    @Column(name = "account_number", nullable = false)
+    private String accountNumber;
+
+    private double balance;
+
+    private boolean active;
+
+    public Transaction() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
 }
