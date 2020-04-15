1f875959f43fa03f728b7231b8e72c7b09aaba86
Mohamed El-Alem <mohamed.el.alem.2017@gmail.com>
2020-04-15 09:54:50 -0500

    Notifications feature (#9)
    * notify admin and teller with teller account creation
    
    * notify user/teller with deposit/withdraw actions

--- src/main/java/com/bym/bankingsystem/services/impl/UserServiceImpl.java
+++ src/main/java/com/bym/bankingsystem/services/impl/UserServiceImpl.java
@@ -5,11 +5,18 @@ import com.bym.bankingsystem.models.auth.User;
 import com.bym.bankingsystem.repositories.RoleRepository;
 import com.bym.bankingsystem.repositories.UserRepository;
 import com.bym.bankingsystem.services.UserService;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.io.Serializable;
+import java.security.Principal;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @Transactional
@@ -18,13 +25,16 @@ public class UserServiceImpl implements UserService {
     private RoleRepository roleRepository;
     private UserRepository userRepository;
     private PasswordEncoder passwordEncoder;
+    private RabbitTemplate rabbitTemplate;
 
     public UserServiceImpl(UserRepository userRepository,
                            RoleRepository roleRepository,
-                           PasswordEncoder passwordEncoder) {
+                           PasswordEncoder passwordEncoder,
+                           RabbitTemplate rabbitTemplate) {
         this.userRepository = userRepository;
         this.roleRepository = roleRepository;
         this.passwordEncoder = passwordEncoder;
+        this.rabbitTemplate = rabbitTemplate;
     }
 
     @Override
@@ -36,15 +46,37 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User createEnabledUser(User user, String roleName) {
+        user.setRawPassword(user.getPassword());
         user.setEnabled(true);
         user.addRole(roleRepository.findOneByName(roleName));
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         userRepository.save(user);
+
+        notifyUserCreation(user, roleName);
         return user;
     }
 
     @Override
     public Optional<User> findByIdAndRole(Long id, String role) {
         return this.userRepository.findByIdAndRole(id, role);
+    }
+
+    private void notifyUserCreation(User user, String role) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String loggedInUsername = ((org.springframework.security.core.userdetails.User) auth.getPrincipal()).getUsername();
+        User loggedInUser = userRepository.findByUsername(loggedInUsername);
+
+        HashMap<String, String> message = new HashMap<>() {{
+           put("createdUserId", user.getId().toString());
+           put("creatingUserId", loggedInUser.getId().toString());
+           put("password", user.getRawPassword());
+        }};
+
+        String exchangeName = "tellerCreation";
+        if (role == "ROLE_USER") {
+            exchangeName = "userCreation";
+        }
+
+        rabbitTemplate.convertAndSend(exchangeName, "", message);
     }
 }
